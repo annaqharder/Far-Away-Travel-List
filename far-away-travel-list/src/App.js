cfa4b1df83013c5f649f0c4de5f34b5ee913c5f3
Anna Harder <annaqharder@gmail.com>
2023-06-26 22:29:37 -0600

    updated stats functionality

--- far-away-travel-list/src/App.js
+++ far-away-travel-list/src/App.js
@@ -4,14 +4,14 @@ import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Sun Hat", quantity: 2, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Sun Hat", quantity: 2, packed: false },
+// ];
 
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   function handleNewItem(item) {
     setItems((items) => [...items, item]);
@@ -37,7 +37,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
--- far-away-travel-list/src/components/Stats.js
+++ far-away-travel-list/src/components/Stats.js
@@ -1,7 +1,22 @@
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Add some items to your packing list!</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentagePacked = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list: You have packed X (X%)</em>{" "}
+      <em>
+        {percentagePacked === 100
+          ? "You've packed everything! Ready to go ✈️"
+          : `You have ${numItems} items on your list: You have packed ${numPacked}
+        items (${percentagePacked}%)`}
+      </em>
     </footer>
   );
 }
